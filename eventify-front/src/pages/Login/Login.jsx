5de403514249a25059d5108dbcc456914d401a0f
inesb <bourhim.ines@gmail.com>
2025-02-11 10:05:20 +0100

    Navigate sur une autre page (liste des events) : OK

--- eventify-front/src/pages/Login/Login.jsx
+++ eventify-front/src/pages/Login/Login.jsx
@@ -1,15 +1,18 @@
 import React, { useState, useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { Form } from "../../components/organisms/index";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
     const { login } = useContext(AuthContext);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const navigate = useNavigate(); 
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        login(email, password);
+        login(email, password, navigate);
     };
 
     return (
